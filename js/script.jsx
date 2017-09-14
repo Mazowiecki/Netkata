136665b1504a08817db56acc4f88790f42c4aad2
Stanisław Mazowiecki <Stmazowiecki@gmail.com>
2017-09-14 20:12:14 +0200

    Done

---
+++ js/script.jsx
@@ -0,0 +1,37 @@
+require('../sass/style.scss');
+
+
+$(document).ready(function(){
+    $(".slider_container").slick({
+        infinite: true,
+        responsive: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: false,
+    });
+
+
+
+
+        $(".news-slider_container").slick({
+            infinite: true,
+            responsive: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            arrows: false,
+            dots: false,
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+});
